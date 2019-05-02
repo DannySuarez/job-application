2845f935b1e4faf333d9958ad2924fef36e80f54
DannySuarez <suarezd10@gmail.com>
2019-05-02 10:31:07 -0700

    reworked api to handle list of orders

--- src/bearDetails-api.js
+++ src/bearDetails-api.js
@@ -1,19 +1,30 @@
 const bearDetailsApi = {
     save(bearDetails) {
-
+        //get an array with order details
+        const allOrders = bearDetailsApi.getAll();
+        allOrders.push(bearDetails);
         //serialize to json
-        const json = JSON.stringify(bearDetails);
+        const json = JSON.stringify(allOrders);
         //save to local storage
-        localStorage.setItem('bearDetails', json);
+        localStorage.setItem('allOrders', json);
     },
     get() {
 
-        //get from local storage
-        const json = localStorage.getItem('bearDetails');
-        //deserialize 
-        const bearDetails = JSON.parse(json);
+        //use get all 
+        const allOrders = bearDetailsApi.getAll();
         //return
-        return bearDetails;
+        return allOrders[0];
+    },
+    getAll() {
+        //get from local storage
+        const json = localStorage.getItem('allOrders');
+        //deserialize 
+        let allOrders = JSON.parse(json);
+        if(!allOrders) {
+            allOrders = [];
+        }
+
+        return allOrders;
     }
 
 };
