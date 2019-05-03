c9935e16cd2d834d78605f3a371c577c9cf08a3f
DannySuarez <suarezd10@gmail.com>
2019-05-03 14:52:03 -0700

    moved code to new file to refactor

--- src/allOrders.js
+++ src/allOrders.js
@@ -1,4 +1,5 @@
 import bearDetailsApi from './bearDetails-api.js';
+import makeOrderRow from './make-order-row.js';
 
 const tbody = document.getElementById('allOrders');
 
@@ -6,39 +7,6 @@ const allOrders = bearDetailsApi.getAll();
 
 for(let i = 0; i < allOrders.length; i++) {
     const order = allOrders[i];
-
-    const tr = document.createElement('tr');
-
-    const nameCell = document.createElement('td');
-
-    const link = document.createElement('a');
-
-    const searchParams = new URLSearchParams();
-
-    searchParams.set('name', order.name);
-
-    link.href = 'orders.html?' + searchParams.toString();
-
-    link.textContent = order.name;
-    nameCell.appendChild(link);
-    tr.appendChild(nameCell);
-
-    const ageCell = document.createElement('td');
-    ageCell.textContent = order.age;
-    tr.appendChild(ageCell);
-
-    const accessoriesCell = document.createElement('td');
-    let accessoriesList = '';
-    if(order.accessories) {
-        accessoriesList = order.accessories.join(', ');
-    }
-    accessoriesCell.textContent = accessoriesList;
-    tr.appendChild(accessoriesCell);
-
-    const moodCell = document.createElement('td');
-    moodCell.textContent = order.mood;
-    tr.appendChild(moodCell);
-
+    const tr = makeOrderRow(order);
     tbody.appendChild(tr);
-
 }
